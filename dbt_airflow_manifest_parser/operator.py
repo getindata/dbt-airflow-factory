9484c7a74248f0f5a005bbc512181d14e5d37c30
p-pekala <piotr.pekala@getindata.com>
2021-11-10 12:59:59 +0100

    Fix model for seed

--- dbt_airflow_manifest_parser/operator.py
+++ dbt_airflow_manifest_parser/operator.py
@@ -17,7 +17,7 @@ from airflow.models.baseoperator import BaseOperator
 
 class DbtRunOperatorBuilder(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def create(self, name, model, command) -> BaseOperator:
+    def create(self, name: str, command: str, model: str = None) -> BaseOperator:
         raise NotImplementedError
 
 
@@ -30,28 +30,34 @@ class KubernetesPodOperatorBuilder(DbtRunOperatorBuilder):
         self.dbt_execution_env_parameters = dbt_execution_env_parameters
         self.kubernetes_execution_parameters = kubernetes_execution_parameters
 
-    def create(self, name: str, model: str, command: str) -> BaseOperator:
-        return KubernetesPodOperator(
-            namespace=self.kubernetes_execution_parameters.namespace,
-            image=self.kubernetes_execution_parameters.image,
-            image_pull_policy=self.kubernetes_execution_parameters.image_pull_policy,
-            cmds=["bash", "-c"],
-            node_selectors=self.kubernetes_execution_parameters.node_selectors,
-            tolerations=self.kubernetes_execution_parameters.tolerations,
-            annotations=self.kubernetes_execution_parameters.annotations,
-            arguments=[
-                f"set -e; "
-                f"dbt --no-write-json {command} "
-                f"--target {self.dbt_execution_env_parameters.target} "
-                f"--models {model} "
-                f"--project-dir {self.dbt_execution_env_parameters.project_dir_path} "
+    def create(self, name: str, command: str, model: str = None) -> BaseOperator:
+        return self._create(self._prepare_arguments(name, command, model))
+
+    def _prepare_arguments(command: str, model: str):
+        args = f"set -e; "
+               f"dbt --no-write-json {command} "
+               f"--target {self.dbt_execution_env_parameters.target} "
+        if model:
+            args += f"--models {model} "
+        args += f"--project-dir {self.dbt_execution_env_parameters.project_dir_path} "
                 f"--profiles-dir {self.dbt_execution_env_parameters.profile_dir_path}"
-            ],
-            labels=self.kubernetes_execution_parameters.labels,
-            name=name,
-            task_id=name,
-            resources=self.kubernetes_execution_parameters.get_resources(),
-            secrets=self.kubernetes_execution_parameters.secrets,
-            is_delete_operator_pod=self.kubernetes_execution_parameters.is_delete_operator_pod,
-            hostnetwork=False,
-        )
+        return [ args ]
+
+    def _create(args):
+        KubernetesPodOperator(
+                    namespace=self.kubernetes_execution_parameters.namespace,
+                    image=self.kubernetes_execution_parameters.image,
+                    image_pull_policy=self.kubernetes_execution_parameters.image_pull_policy,
+                    cmds=["bash", "-c"],
+                    node_selectors=self.kubernetes_execution_parameters.node_selectors,
+                    tolerations=self.kubernetes_execution_parameters.tolerations,
+                    annotations=self.kubernetes_execution_parameters.annotations,
+                    arguments=args,
+                    labels=self.kubernetes_execution_parameters.labels,
+                    name=name,
+                    task_id=name,
+                    resources=self.kubernetes_execution_parameters.get_resources(),
+                    secrets=self.kubernetes_execution_parameters.secrets,
+                    is_delete_operator_pod=self.kubernetes_execution_parameters.is_delete_operator_pod,
+                    hostnetwork=False,
+                )
