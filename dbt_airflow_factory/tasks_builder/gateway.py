02863275b4042c24c874e6e3075a94a6eaaa90fa
Pawel Kocinski <pawel.kocinski@getindata.com>
2022-07-29 10:57:44 +0200

    feat: [DATA-412] Fix code complexity.

---
+++ dbt_airflow_factory/tasks_builder/gateway.py
@@ -0,0 +1,65 @@
+from typing import Any, Dict, List
+
+from dbt_airflow_factory.tasks_builder.utils import is_model_run_task
+
+
+def is_gateway_valid_dependency(
+    dependency_node_name: str,
+    manifest: dict,
+    node: Dict[str, Any],
+    dataset_left: str,
+    dataset_right: str,
+) -> bool:
+    if is_model_run_task(dependency_node_name):
+        dependency_node = manifest["nodes"][dependency_node_name]
+
+        if dependency_node["schema"] == dataset_left and node["schema"] == dataset_right:
+            return False
+        return True
+    return True
+
+
+def get_gateway_dependencies(
+    manifest: dict, separation_layer_left: str, separation_layer_right: str
+) -> List:
+    downstream_dependencies = _get_downstream_dependencies(
+        manifest=manifest, separation_layer_right=separation_layer_right
+    )
+
+    upstream_dependencies_connected_to_downstream = (
+        _get_upstream_dependencies_connected_to_downstream(
+            manifest=manifest,
+            separation_layer_left=separation_layer_left,
+            downstream_dependencies=downstream_dependencies,
+        )
+    )
+    dependencies = [
+        node_name
+        for node_name, values in manifest["nodes"].items()
+        if values["schema"] == separation_layer_left
+        and node_name in upstream_dependencies_connected_to_downstream
+    ]
+    return dependencies
+
+
+def _get_downstream_dependencies(manifest: dict, separation_layer_right: str) -> List:
+    downstream_dependencies = [
+        node_name
+        for node_name, values in manifest["nodes"].items()
+        if values["schema"] == separation_layer_right
+    ]
+    return downstream_dependencies
+
+
+def _get_upstream_dependencies_connected_to_downstream(
+    manifest: dict, separation_layer_left: str, downstream_dependencies: List
+) -> List:
+    upstream_dependencies_connected_to_downstream = []
+
+    for downstream_node in downstream_dependencies:
+        upstream_deps = manifest["nodes"][downstream_node]["depends_on"]["nodes"]
+        for dep in upstream_deps:
+            if is_model_run_task(dep):
+                if manifest["nodes"][dep]["schema"] == separation_layer_left:
+                    upstream_dependencies_connected_to_downstream.append(dep)
+    return upstream_dependencies_connected_to_downstream
