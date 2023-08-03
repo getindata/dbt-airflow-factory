71706bd9567f085c91bb0de4b4d7560ecb6795ff
Piotr Tutak <piotr.tutak@getindata.com>
2023-08-03 15:36:31 +0200

    max line length

--- dbt_airflow_factory/tasks_builder/parameters.py
+++ dbt_airflow_factory/tasks_builder/parameters.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class TasksBuildingParameters:
-    use_task_group: bool = True,
-    show_ephemeral_models: bool = True,
-    enable_dags_dependencies: bool = False,
-    check_all_deps_for_multiple_deps_tests: bool = False,
+    use_task_group: bool = True
+    show_ephemeral_models: bool = True
+    enable_dags_dependencies: bool = False
+    check_all_deps_for_multiple_deps_tests: bool = False
