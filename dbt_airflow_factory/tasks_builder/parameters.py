bf898446093f46b539c38ac372b874d9368a96d2
ptutak-getindata <133227844+ptutak-getindata@users.noreply.github.com>
2023-08-04 12:21:45 +0200

    update dbt graph builder (#100)
    * update dbt graph builder
    
    * update
    
    * version update
    
    * max line length
    
    * line lenght
    
    * update versions
    
    * better versioning
    
    * enable config loading
    
    * version bump
    
    * proper versions
    
    * Default airflow config True

--- dbt_airflow_factory/tasks_builder/parameters.py
+++ dbt_airflow_factory/tasks_builder/parameters.py
@@ -1,10 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
 class TasksBuildingParameters:
-    def __init__(
-        self,
-        use_task_group: bool = True,
-        show_ephemeral_models: bool = True,
-        enable_dags_dependencies: bool = False,
-    ) -> None:
-        self.use_task_group = use_task_group
-        self.show_ephemeral_models = show_ephemeral_models
-        self.enable_dags_dependencies = enable_dags_dependencies
+    use_task_group: bool = True
+    show_ephemeral_models: bool = True
+    enable_dags_dependencies: bool = False
+    check_all_deps_for_multiple_deps_tests: bool = False
